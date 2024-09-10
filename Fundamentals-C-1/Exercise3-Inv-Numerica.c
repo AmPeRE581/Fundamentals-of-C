6068a118a07ac5f3be0f64e67918cec980eb9fef
AmPeRE581 <francesco.franca75@gmail.com>
2024-09-10 18:13:59 +0200

    Exercise3 Completed --

---
+++ Fundamentals-C-1/Exercise3-Inv-Numerica.c
@@ -0,0 +1,25 @@
+/*Esercizio1_3: Leggere due numeri interi a e b ed invertirli (cioè, fare sì che a fine programma il contenuto di a
+sia il precedente contenuto di b e viceversa). Esempio: se a=3 e b=102, scrivere le opportune istruzioni a=102 e b=3.*/
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+
+    printf("Inserisci il numero (a): ");
+    scanf("%d", &a);
+
+    printf("Inserisci il numero (b): ");
+    scanf("%d", &b);
+
+    /*Scambio di valori*/
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("Dopo lo scambio, a=%d e b = %d\n", a, b);
+
+    return 0;
+
+}
