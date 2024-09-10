735129451577f53c9fe1a0d0b347a20f39ce003f
AmPeRE581 <francesco.franca75@gmail.com>
2024-09-10 12:05:57 +0200

    -- Exercise2 Completed

--- Fundamentals-C-1/Exercise2-Numeri-Concorde.c
+++ Fundamentals-C-1/Exercise2-Numeri-Concorde.c
@@ -1,3 +1,21 @@
 /*Esercizio1_2: Leggere due numeri interi x e y (si assume siano diversi da zero):
 stampare il messaggio 'numeri di segno concorde' se x e y hanno lo stesso segno.
 Esempio: se x=3 e y=9 il programma stampa 'numeri di segno concorde' e termina; se x=2 e y=-7 il programma termina direttamente.*/
+
+#include<stdio.h>
+
+int main(){
+    int x,y;
+
+    printf("Dammi il valore di (x): ");
+    scanf("%d", &x);
+
+    printf("Dammi il valore di (y): ");
+    scanf("%d", &y);
+
+    if((x > 0 && y > 0) || (x < 0 && y < 0)){
+        printf("Numeri sono di segno concorde...");
+    }
+
+    return 0;
+}
