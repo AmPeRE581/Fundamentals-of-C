3158d8dc3964b414c7a5a521f9234d1244efaad0
AmPeRE581 <francesco.franca75@gmail.com>
2024-09-19 15:28:39 +0200

    ----

---
+++ Fundamentals-C-1/Funzioni-C/Exercise4-Funzione-Float.c
@@ -0,0 +1,21 @@
+/*Esercizio1_4: Esempio: Scrivi un programma che, dati un prezzo e uno sconto, calcola il prezzo scontato.*/
+
+#include<stdio.h>
+
+float sconto(float prezzo_originale, float sconto){
+    float prezzo scontato;
+    prezzo scontato=prezzo*sconto;
+    return prezzo_scontato;
+}
+
+int main(){
+    float p,s;
+    printf("Inserire prezzo da scontare");
+    scanf("%f", &p);
+
+    printf("Inserire Sconto"); //es. 0.5 corrisponde al 50%
+    scanf("%f",&s);
+    
+    float ris=sconto(p,s);
+    printf("Prezzo scontato: %f",ris);
+}
