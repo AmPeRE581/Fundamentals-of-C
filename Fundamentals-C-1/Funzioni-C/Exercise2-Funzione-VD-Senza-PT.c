3449c0df0adc2ecd91c790f89f7580c80b1aa01a
AmPeRE581 <francesco.franca75@gmail.com>
2024-09-20 15:52:16 +0200

    ------

--- Fundamentals-C-1/Funzioni-C/Exercise2-Funzione-VD-Senza-PT.c
+++ Fundamentals-C-1/Funzioni-C/Exercise2-Funzione-VD-Senza-PT.c
@@ -1,21 +1,21 @@
 /*Esercizio1_2: Funzioni VOID Senza Parametri*/
 
-#include<stdio.h>
+#include <stdio.h>
 
 void benvenuto(){
     printf("Ciao!\n");
-    printf("Avvio Programma");
+    printf("Avvio Programma\n");
 }
 
 void arrivederci(){
     printf("\nProgramma terminato\n");
-    printf("Addio!");
+    printf("Addio!\n");
 }
 
 int main(){
     benvenuto();
     int n,i;
-    printf("Dammi un valore\n");
+    printf("Dammi un valore:\n");
     scanf("%d", &n);
     for(i=0; i<n; i++){
         printf("*");
