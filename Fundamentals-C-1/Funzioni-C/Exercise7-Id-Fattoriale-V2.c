764500c091a4950b7e6cd57bf5e72f02a1440c6b
AmPeRE581 <francesco.franca75@gmail.com>
2024-09-21 15:52:04 +0200

    ----

--- Fundamentals-C-1/Funzioni-C/Exercise7-Id-Fattoriale-V2.c
+++ Fundamentals-C-1/Funzioni-C/Exercise7-Id-Fattoriale-V2.c
@@ -1,6 +1,9 @@
 /*Scrivere un programma in c che scrive una funzione che calcola e restituisce il fattoriale di un numeri intero n*/ 
 /*Versione 2.0*/
 
+/*Spiegazione: L'istruzione "return-espressione", provoca la terminazione della funzione e la restituzione del controllo al chiamante
+unitamente al valore dell'espressione. Eventuali istruzioni successive alla "return" non saranno mai eseguite. */
+
 #include <stdio.h>
 
 int fattoriale_v2(int n){
