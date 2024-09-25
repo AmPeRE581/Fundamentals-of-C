a14948e44d90946331d717e992f667b4330db5fe
Francesco Francavilla <francesco.franca75@gmail.com>
2024-09-25 11:29:59 +0200

    -- Exercises0-6 Added --

---
+++ Fundamentals-C-1/Funzioni-C/Exercise0-6-Double-Funzione.c
@@ -0,0 +1,20 @@
+/*Funzione media riceve un array di interi (x) ed il suo riempimento (n) e restituisce la media calcolata su gli elementi di x*/
+
+double media(int x[], int n){
+    int somma=0, i;
+    for(i=0; i<n; i++){
+        somma=somma+x[i];
+
+        return (double) somma\n;
+    }
+}
+
+/*La funzione scriviSe riceve un array di interi (x) ed il suo riempimento (n) e scrive
+gli elementi dell'array maggiori della media degli elementi di x (usa la funzione media per il calcolo della media degli elementi dell'array)*/
+
+void scriviSe(int x[], int n){
+    double media_vett=media(x,n);
+    int i;
+
+    printf("Scrittura degli elementi del vettore maggiori della media(%g)\n", media_vett);
+}
