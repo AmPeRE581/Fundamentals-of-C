3d4f2dbfa6f74f7e3e10e26a7f74ffa4ba960995
Francesco Francavilla <francesco.franca75@gmail.com>
2024-10-09 09:59:03 +0200

    ----

--- Fundamentals-C-1/Programmazione-Varia-C/Esempi-Svolgimento-C/Esercizio11-Esempio11.c
+++ Fundamentals-C-1/Programmazione-Varia-C/Esempi-Svolgimento-C/Esercizio11-Esempio11.c
@@ -6,3 +6,48 @@ Esempio di sequenze bitoniche:
 
 -- 3 5 9 13 121 145 66 30 8 2
 -- 1 3 7 1025 82 60 48*/
+
+#include <stdio.h>
+
+int main(){
+    int n,i;
+    printf("Inserisci il numero di elementi della sequenza: ");
+    scanf("%d", &n);
+
+    int numeri[n];
+    printf("Inserisci gli elementi della sequenza:\n");
+    for(i=0; i<n; i++){
+        scanf("%d", &numeri[i]);
+    }
+
+    int crescente = 1; /*Flag per indicare se siamo posizionati nella parte crescente*/
+    int dispari = numeri[0] % 2; /*Memorizziamo la parità del primo numero*/
+
+    for(i=1; i<n; i++){
+        if (dispari){
+            /*Qui ci si trova nel punto decrescente....*/
+            if(numeri[i] %2 ==0 || numeri[i] <= numeri[i-1]){
+                crescente=0;
+
+                break;
+            }
+        } else {
+
+            /*Qui ci si trova nel punto crescente...*/
+            if(numeri[i]%2 ==1 || numeri[i] >= numeri[i-1]){
+                crescente = 0;
+
+                break;
+            }
+        }
+        dispari = numeri[i]%2;
+    }
+
+    if(crescente){
+        printf("La sequenza è bitonica.\n");
+    } else {
+        printf("La sequenza non è bitonica.\n");
+    }
+
+    return 0;
+}
