3a5818482720488fe54de2e6338fd40d0029092f
Francesco Francavilla <francesco.franca75@gmail.com>
2024-10-13 16:02:35 +0200

    ----

---
+++ Fundamentals-C-1/Programmazione-Varia-C/Esempi-Svolgimento-C/Esercizio17-Esempio17.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n){
+    int i,j, temp;
+    for(i=0; i<n-1; i++){
+        for(j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                temp=arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size){
+    int i;
+    for(i=0; i<size; i++){
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n;
+    printf("Inserisci il numero di elementi dell'array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Inserisci %d numeri interi: \n", n);
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+    printf("Array ordinato:\n");
+    printArray(arr, n);
+    return 0;
+}
