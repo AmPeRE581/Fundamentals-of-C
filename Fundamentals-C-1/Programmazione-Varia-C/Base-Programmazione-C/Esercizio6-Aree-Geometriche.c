05e0d0ab8849784d481fb373aa2d5a2af8c44c39
Francesco Francavilla <francesco.franca75@gmail.com>
2024-10-15 11:05:32 +0200

    ----

---
+++ Fundamentals-C-1/Programmazione-Varia-C/Base-Programmazione-C/Esercizio6-Aree-Geometriche.c
@@ -0,0 +1,29 @@
+/*Esercizio6: Si scriva un programma in linguaggio C che, dato un numero reale D immesso da tastiera,
+calcoli e stampi:
+1. l’area del quadrato di lato D
+2. l’area del cerchio di diametro D
+3. l’area del triangolo equilatero di lato D*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float D, area_quadrato, area_triangolo, area_cerchio;
+    const float pi = 3.14159;
+
+    printf("Inserisci la lunghezza del parametro D: ");
+    scanf("%f", &D);
+
+    /*Da qui comincia la logica, per il calcolo delle aree*/
+
+    area_quadrato = D*D;
+    area_cerchio = pi * pow(D/2, 2); /*Funzione POW...utile per consentire l'elevazione*/
+    area_triangolo = sqrt(3) / 4*D*D;
+
+    /*E infine si stampano i risultati...e sono i seguenti: */
+
+    printf("L'area del quadrato è: %f\n", area_quadrato);
+    printf("L'area del cerchio è: %f\n", area_cerchio);
+    printf("L'area del triangolo è: %f\n", area_triangolo);
+}
+
