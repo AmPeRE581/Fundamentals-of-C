ce9f6fe1d5e30427682cacffe9a204e16a74a0f2
Francesco Francavilla <francesco.franca75@gmail.com>
2024-10-30 15:39:24 +0100

    -- Esercizio46/47 Completed --

---
+++ Fundamentals-C-1/Programmazione-Varia-C/Base-Programmazione-C/Esercizio46-Stampa-Num-Standard.c
@@ -0,0 +1,51 @@
+/*Scrivere ed eseguire un programma C opportunamente suddiviso in funzioni che letta in input una sequenza di 10 numeri, calcoli e stampi su standard output:
+ -- la media dei numeri letti (come divisione tra la somma dei numeri e 10)
+ -- il numero massimo tra i numeri letti
+Inoltre, siano ME la media e MA il massimo, per ogni numero X della sequenza, calcolare e stampare su standard output:
+ -- la differenza ME - X
+ -- la differenza MA - X*/
+
+#include <stdio.h>
+#define NUM_ELEMENTI 10
+
+/*Funzione per calcolare la media di un array di numeri*/
+float calcolaMedia(int numeri[]){
+    int somma=0; 
+    for(int i=0; i<NUM_ELEMENTI; i++){
+        somma += numeri[i];
+    }
+    return (float)somma / NUM_ELEMENTI;
+}
+
+/*Funzione per trovare il massimo valore di un array di numeri*/
+int trovaMassimo(int numeri[]){
+    int massimo = numeri[0];
+    for(int i=1; i<NUM_ELEMENTI; i++){
+        massimo = numeri[i];
+    }
+    return massimo;
+}
+
+int main(){
+    int numeri[NUM_ELEMENTI];
+    int i;
+
+    printf("Inserisci %d numeri:\n", NUM_ELEMENTI);
+    for(i=0; i<NUM_ELEMENTI; i++){
+        scanf("%d", &numeri[i]);
+    }
+
+    float media = calcolaMedia(numeri);
+    int massimo = trovaMassimo(numeri);
+
+    printf("\nLa media è: %.2f\n", media);
+    printf("Il massimo è: %d\n\n", massimo);
+
+    for(i=0; i<NUM_ELEMENTI; i++){
+        printf("Numero: %d\n", numeri[i]);
+        printf("Differenza media-numero: %.2f\n", media - numeri[i]);
+        printf("Differenza massimo-numero: %d\n\n", massimo - numeri[i]);
+    }
+
+    return 0;
+}
---
+++ Fundamentals-C-1/Programmazione-Varia-C/Base-Programmazione-C/Esercizio47-Somma-Palindromo.c
@@ -0,0 +1,32 @@
+/*Scrivere ed eseguire un programma C++ opportunamente suddiviso in funzioni che letta in input una sequenza di 10 caratteri, stampi su standard output “SI” se la sequenza è palindroma, “NO” altrimenti.
+Una sequenza di caratteri si dice palindroma se, letta al contrario, rimane invariata. Ad esempio, sono palindrome le sequenze: “abcbaabcba”, “aeiouuoiea”.*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LUNGHEZZA 10
+
+/*Funzione per verificare se una stringa è palindroma*/
+int isPalindroma(char stringa[]){
+    int lunghezza = strlen(stringa);
+    for(int i=0; i<lunghezza / 2; i++){
+        if(stringa[i] != stringa[lunghezza - i - 1]){
+            return 0; /*Non è palindroma*/
+        }
+    }
+
+    return 1; /*E' palindromo*/
+}
+
+int main(){
+    char stringa[MAX_LUNGHEZZA];
+
+    printf("Inserisci una sequenza di %d caratteri: ", MAX_LUNGHEZZA);
+    scanf("%s", stringa);
+
+    if(isPalindroma(stringa)){
+        printf("SI\n");
+    } else {
+        printf("NO\n");
+    }
+}
